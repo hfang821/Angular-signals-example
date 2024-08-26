d5c2a873588361edb60d05e673a81785040aa140
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-26 17:53:50 -0400

    add another use case of computed signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { signal, effect, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, CommonModule]
 })
 export class AppComponent {
   title = 'angular-signals-exmaple';
@@ -38,6 +41,26 @@ export class AppComponent {
 
   changeQuantity(event: Event){
     this.quantity.set((event.target as HTMLInputElement).valueAsNumber)
+  }
+
+
+  products = signal([
+    {id: 1, name: 'Milk', price: 2.99},
+    {id: 2, name: 'Bread', price: 1.99},
+    {id: 3, name: 'Eggs', price: 3.99},
+  ])
+
+  filterName = signal('');
+
+  filteredProducts = computed(() => {
+    return this.products().filter(product => product.name
+      .toLocaleLowerCase()
+      .includes(this.filterName().toLocaleLowerCase()))
+  });
+
+  changeFilter(event: Event) {
+    let newFilterName = (event.target as HTMLInputElement).value;
+    this.filterName.set(newFilterName);
   }
 
 }
